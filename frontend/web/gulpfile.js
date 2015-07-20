10ea10d77fbc6898d4c772b939decc32da1f14fb
pazhapn <pazhapn@gmail.com>
2015-07-20 09:09:12 -0400

    y

--- frontend/web/gulpfile.js
+++ frontend/web/gulpfile.js
@@ -1,33 +1,12 @@
-/**
- * Created by pazha on 3/22/2015.
- */
-var gulp = require('gulp'),
-  clean = require('gulp-clean'),
-  include = require('gulp-include'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  rename = require('gulp-rename'),
-  filesize = require('gulp-filesize'),
-  less = require('gulp-less'),
-  changed = require('gulp-changed'),
-  typescript = require("gulp-typescript"),
-  sourcemaps = require("gulp-sourcemaps"),
-  plumber = require('gulp-plumber'),
-  haml = require('gulp-ruby-haml'),
-  sass = require('gulp-ruby-sass'),
-  minifyCSS = require('gulp-minify-css'),
-  neat = require('node-neat').includePaths,
-  //reactTemplates = require("gulp-react-templates"),
-  //babel = require('gulp-babel'),
-  //traceur = require('gulp-traceur'),
-  //es6Path = 'es6/*.js',
-  //rtPath = 'src/rt/**/*.rt',
-  tsPath = ['ts/*.ts'],
-  jsPath = ['js/site/*.js'],
-  sassPath = 'src/scss/',
-  requirePath = 'src/require/*.js',
-  compilePath = 'build/',
-  cssPath = compilePath + 'css',
+var gulp = require('gulp'), clean = require('gulp-clean'), include = require('gulp-include'),
+  concat = require('gulp-concat'), uglify = require('gulp-uglify'), rename = require('gulp-rename'),
+  filesize = require('gulp-filesize'), less = require('gulp-less'), changed = require('gulp-changed'),
+  typescript = require("gulp-typescript"), sourcemaps = require("gulp-sourcemaps"),
+  plumber = require('gulp-plumber'), haml = require('gulp-ruby-haml'), sass = require('gulp-ruby-sass'),
+  minifyCSS = require('gulp-minify-css'), neat = require('node-neat').includePaths,
+  tsPath = ['src/ts/**/*.ts'], jsPath = ['src/js/site/*.js'], sassPath = 'src/scss/',
+  requirePath = 'src/require/*.js', compilePath = 'build/', cssCompilePath = compilePath + 'css',
+  jsCompilePath = compilePath + 'js',
   delFiles = [compilePath, 'static/scripts/*.min.js', compilePath+'css'];
 
 gulp.task('clean', function() {
@@ -47,10 +26,11 @@ gulp.task('sass', function() {
           console.error('Error', err.message);
         });
 });
+/*
 gulp.task('js', function() {
-  return gulp.src(jsPath)
+  return gulp.src(jsCompilePath)
   .pipe(plumber())
-  .pipe(concat('app.un.js'))
+  .pipe(concat('home.un.js'))
   .pipe(gulp.dest('static/scripts'))
   .pipe(filesize())
   .pipe(uglify())
@@ -59,6 +39,7 @@ gulp.task('js', function() {
   .pipe(filesize())
   .pipe(plumber.stop());
 });
+*/
 gulp.task('require', function() {
   return gulp.src(requirePath)
   .pipe(plumber())
@@ -67,12 +48,18 @@ gulp.task('require', function() {
   .pipe(gulp.dest('static/scripts'))
   .pipe(plumber.stop());
 });
+
 gulp.task('typescript', function() {
-  gulp.src(tsPath)
+  var tsresult = gulp.src(tsPath)
   .pipe(plumber())
   .pipe(sourcemaps.init())
-  .pipe(typescript())
-  .pipe(concat('home.un.js'))
+  .pipe(typescript({sortOutput: true}));
+  return tsresult.js
+  .pipe(concat('app.un.js'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('static/scripts'))
+  .pipe(rename('app.min.js'))
+  .pipe(uglify())
   .pipe(gulp.dest('static/scripts'))
   .pipe(plumber.stop())
   .on('error', function (err) {
@@ -81,64 +68,13 @@ gulp.task('typescript', function() {
 });
 
 gulp.task('watch', function() {
-  //gulp.watch([es6Path], ['traceur', 'babel']);
   gulp.watch(tsPath, ['typescript']);
-  gulp.watch(jsPath, ['js']);
-  gulp.watch(cssPath, ['css']);
+  //gulp.watch(jsPath, ['js']);
+  gulp.watch(cssCompilePath, ['css']);
   gulp.watch(sassPath+"*.scss", ['sass']);
   gulp.watch(requirePath, ['require']);
 });
 
 //gulp.task('default', ['traceur', 'babel', 'watch']);
-gulp.task('default', ['watch', 'typescript', 'js', 'sass', 'require']);
-
-
-/*
-gulp.task('traceur', function () {
-  gulp.src([es6Path])
-      .pipe(plumber())
-      .pipe(traceur({ blockBinding: true }))
-      .pipe(gulp.dest(compilePath + '/traceur'));
-});
-gulp.task('babel', function () {
-gulp.src([es6Path])
-.pipe(sourcemaps.init())
-.pipe(plumber())
-.pipe(babel())
-.pipe(sourcemaps.write("."))
-.pipe(gulp.dest(compilePath + '/babel'));
-});
-gulp.task('reactTemplates', function() {
-  gulp.src(rtPath)
-    .pipe(sourcemaps.init())
-    .pipe(plumber())
-    .pipe(reactTemplates())
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest(compilePath + 'rt'));
-});
-
-gulp.task('sass', function() {
-  return gulp.src(sassPath)
-    .pipe(sass())
-    .pipe(gulp.dest(compilePath+'css'))
-    .on('error', function (err) {
-      console.error('Error', err.message);
-    });
-});
-gulp.task('css', function() {
-    return gulp.src(cssPath)
-    .pipe(plumber())
-    .pipe(concat('application.css'))
-    .pipe(gulp.dest(compilePath+'/css'))
-    .pipe(filesize())
-    .pipe(uglify())
-    .pipe(rename('app.min.css'))
-    .pipe(gulp.dest('static/css'))
-    .pipe(filesize())
-    .pipe(plumber.stop())
-    .on('error', function (err) {
-      console.error('Error', err.message);
-    });
-});
-
-*/
+//gulp.task('default', ['watch', 'typescript', 'js', 'sass', 'require']);
+gulp.task('default', ['watch', 'typescript', 'sass', 'require']);
